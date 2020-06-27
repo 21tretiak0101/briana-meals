208233973d50a2429754c34b12eaa1213ce795b4
21tretiak0101 <ilia.tretiak.2001@mail.ru>
2020-06-27 17:42:31 +0300

    test: add service tests

---
+++ src/test/java/by/ttre16/enterprise/service/util/TestData.java
@@ -0,0 +1,7 @@
+package by.ttre16.enterprise.service.util;
+
+public class TestData {
+    public static final Integer USER_ID = 1;
+    public static final Integer ADMIN_ID = 2;
+    public static final Integer NOT_FOUND_ID = 500;
+}
