b7a4a5162d26c18b3671af7c6aeded6c8a8f242f
21tretiak0101 <ilia.tretiak.2001@mail.ru>
2020-07-05 23:42:58 +0300

    feat: update persistence entities

--- src/main/java/by/ttre16/enterprise/model/AbstractNamedEntity.java
+++ src/main/java/by/ttre16/enterprise/model/AbstractNamedEntity.java
@@ -1,11 +1,27 @@
 package by.ttre16.enterprise.model;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@MappedSuperclass
 public abstract class AbstractNamedEntity extends AbstractBaseEntity {
+    @NotEmpty
+    @Size(min = 5, max = 255)
+    @Column(name = "name")
     protected String name;
 
     public AbstractNamedEntity(Integer id, String name) {
         super(id);
         this.name = name;
+    }
+
+    public AbstractNamedEntity(String name) {
+        this.name = name;
+    }
+
+    public AbstractNamedEntity(Integer id) {
+        super(id);
     }
 
     public AbstractNamedEntity() { }
--- src/main/java/by/ttre16/enterprise/model/Meal.java
+++ src/main/java/by/ttre16/enterprise/model/Meal.java
@@ -1,18 +1,63 @@
 package by.ttre16.enterprise.model;
 
+import org.hibernate.validator.constraints.Range;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Objects;
 
-public class Meal extends AbstractBaseEntity{
+@Entity
+@Table(name = "meals")
+@NamedQueries({
+    @NamedQuery(name = Meal.GET_ALL,
+            query = "select m from Meal m where m.user.id=:userId"),
+    @NamedQuery(name = Meal.DELETE_ALL,
+            query = "delete from Meal m where m.user.id=:userId"),
+    @NamedQuery(name = Meal.DELETE_ONE,
+            query = "delete from Meal m " +
+                    "where m.id=:mealId and m.user.id=:userId"),
+})
+public class Meal extends AbstractBaseEntity {
+    public static final String GET_ALL = "Meal.getAll";
+    public static final String DELETE_ONE = "Meal.deleteOne";
+    public static final String DELETE_ALL = "Meal.deleteAll";
+
+    @Column(name = "calories")
+    @Range(min = 10, max = 5000)
+    @NotNull
     private Integer calories;
+
+    @Column(name = "date_time", nullable = false)
+    @NotNull
     private LocalDateTime dateTime;
+
+    @Column(name = "description", nullable = false)
+    @Size(min = 2, max = 120)
+    @NotBlank
     private String description;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = {CascadeType.ALL})
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 
     public Meal() { }
 
-    public Meal(Integer id, Integer calories,
-                LocalDateTime dateTime, String description) {
+    public Meal(Integer id, Integer calories, LocalDateTime dateTime,
+                String description, User user) {
+        super(id);
+        this.id = id;
+        this.calories = calories;
+        this.dateTime = dateTime;
+        this.description = description;
+        this.user = user;
+    }
+
+    public Meal(Integer id, Integer calories, LocalDateTime dateTime,
+                String description) {
         super(id);
         this.id = id;
         this.calories = calories;
@@ -21,7 +66,7 @@ public class Meal extends AbstractBaseEntity{
     }
 
     public Meal(Meal meal) {
-        this(meal.id, meal.calories, meal.dateTime, meal.description);
+        this(meal.id, meal.calories, meal.dateTime, meal.description, meal.user);
     }
 
     public void setCalories(Integer calories) {
@@ -34,6 +79,14 @@ public class Meal extends AbstractBaseEntity{
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
     }
 
     public Integer getCalories() {
