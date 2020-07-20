86e628146d1de13900bc8cc35a8ea723387a26f3
21tretiak0101 <ilia.tretiak.2001@mail.ru>
2020-07-21 00:47:30 +0300

    chore: add dependency management configuration

---
+++ src/main/java/by/ttre16/enterprise/util/ProfileUtil.java
@@ -0,0 +1,15 @@
+package by.ttre16.enterprise.util;
+
+public class ProfileUtil {
+    public static final String DATA_JPA = "dataJpa";
+
+    public static final String JDBC = "jdbc";
+
+    public static final String IN_MEMORY = "inMemory";
+
+    public static final String JPA = "jpa";
+
+    public static final String DEVELOPMENT = "development";
+
+    public static final String TEST = "test";
+}
