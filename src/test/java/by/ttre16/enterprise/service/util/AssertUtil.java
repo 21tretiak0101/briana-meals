1ec11d9a1ec8416b5480cd5482f1dd0dc2946002
21tretiak0101 <ilia.tretiak.2001@mail.ru>
2020-08-03 22:51:09 +0300

    test: add recursive comparing configuration

--- src/test/java/by/ttre16/enterprise/service/util/AssertUtil.java
+++ src/test/java/by/ttre16/enterprise/service/util/AssertUtil.java
@@ -1,25 +1,50 @@
 package by.ttre16.enterprise.service.util;
 
 import by.ttre16.enterprise.util.exception.NotFoundException;
+import org.assertj.core.api.recursive.comparison.RecursiveComparisonConfiguration;
 import org.junit.function.ThrowingRunnable;
+import org.slf4j.Logger;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertThrows;
+import static org.slf4j.LoggerFactory.getLogger;
 
 public class AssertUtil {
-    public static <T> void assertMatch(Iterable<T> expected,
-            Iterable<T> actual, String ...ignoring) {
-        assertThat(actual)
-                .usingElementComparatorIgnoringFields(ignoring)
-                .containsExactlyInAnyOrderElementsOf(expected);
-    }
+    private static final Logger log = getLogger("result");
 
     public static <T> void assertMatch(T expected, T actual,
             String ...ignoring) {
-        assertThat(actual).isEqualToIgnoringGivenFields(expected, ignoring);
+        RecursiveComparisonConfiguration recursiveComparisonConfiguration =
+                new RecursiveComparisonConfiguration();
+        recursiveComparisonConfiguration.ignoreCollectionOrder(true);
+        recursiveComparisonConfiguration.ignoreFields(ignoring);
+        assertThat(actual)
+                .usingRecursiveComparison(recursiveComparisonConfiguration)
+                .isEqualTo(expected);
     }
 
     public static void assertThrowsNotFoundException(ThrowingRunnable tr) {
         assertThrows(NotFoundException.class, tr);
+    }
+
+    public static <T extends Throwable> void validateRootCause(
+            Runnable runnable, Class<T> rootExceptionClass) {
+        assertThrows(rootExceptionClass, () -> {
+            try {
+                runnable.run();
+            } catch (Exception e) {
+                throw getRootCause(e);
+            }
+        });
+    }
+
+    public static Throwable getRootCause(Throwable throwable) {
+        Throwable result = throwable;
+        Throwable cause;
+        while ((cause = result.getCause()) != null && (result != cause)) {
+            result = cause;
+        }
+        log.warn("Exception root cause: ", cause);
+        return result;
     }
 }
