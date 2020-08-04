ea7b843f3e4e2638413f726e5cddc3edecad4e8f
21tretiak0101 <ilia.tretiak.2001@mail.ru>
2020-08-04 14:06:52 +0300

    chore: update rest controllers

--- src/main/java/by/ttre16/enterprise/controller/rest/MealRestController.java
+++ src/main/java/by/ttre16/enterprise/controller/rest/MealRestController.java
@@ -1,14 +1,15 @@
-package by.ttre16.enterprise.controller;
+package by.ttre16.enterprise.controller.rest;
 
 import by.ttre16.enterprise.dto.MealTo;
 import by.ttre16.enterprise.model.Meal;
 import by.ttre16.enterprise.service.MealService;
-import by.ttre16.enterprise.util.SecurityUtil;
+import by.ttre16.enterprise.security.SecurityUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.Nullable;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -17,48 +18,50 @@ import java.util.List;
 import static by.ttre16.enterprise.util.DateTimeUtil.atEndOfDayOrMax;
 import static by.ttre16.enterprise.util.DateTimeUtil.atStartOfDayOrMin;
 import static by.ttre16.enterprise.util.MealUtil.getMealsWithExcess;
-import static by.ttre16.enterprise.util.SecurityUtil.authUserCaloriesPerDay;
 import static by.ttre16.enterprise.util.ValidationUtil.assureIdConsistent;
 import static by.ttre16.enterprise.util.ValidationUtil.checkNew;
 
-@Controller
+@RestController
+@RequestMapping("/rest/meals")
 public class MealRestController {
     protected final Logger log = LoggerFactory.getLogger(getClass());
 
     private final MealService service;
+    private final SecurityUtil securityUtil;
 
     @Autowired
-    public MealRestController(MealService service) {
+    public MealRestController(MealService service, SecurityUtil securityUtil) {
         this.service = service;
+        this.securityUtil = securityUtil;
     }
 
     public List<Meal> getAll() {
-        int userId = SecurityUtil.authUserId();
+        Integer userId = getUserId();
         log.info("getAll");
         return service.getAllByUserId(userId);
     }
 
     public Meal get(int id) {
-        int userId = SecurityUtil.authUserId();
+        Integer userId = getUserId();
         log.info("get {}", id);
         return service.getOne(userId, id);
     }
 
     public void create(Meal meal) {
-        int userId = SecurityUtil.authUserId();
+        Integer userId = getUserId();
         log.info("create {}", meal);
         checkNew(meal);
         service.save(userId, meal);
     }
 
     public void delete(Integer id) {
-        int userId = SecurityUtil.authUserId();
+        Integer userId = getUserId();
         log.info("delete {}", id);
         service.delete(userId, id);
     }
 
     public void update(Meal meal, Integer id) {
-        int userId = SecurityUtil.authUserId();
+        Integer userId = getUserId();
         assureIdConsistent(meal, id);
         log.info("update {} with id={}", meal, id);
         service.update(meal, userId);
@@ -67,7 +70,7 @@ public class MealRestController {
     public List<MealTo> getBetween(@Nullable LocalDate startDate,
             @Nullable LocalDate endDate,@Nullable LocalTime startTime,
             @Nullable LocalTime endTime) {
-        int userId = SecurityUtil.authUserId();
+        Integer userId = getUserId();
         log.info("getBetween dates({} - {}) time({} - {}) for user {}",
                 startDate, endDate, startTime, endTime, userId);
         List<Meal> mealsDateFiltered = service.getBetweenInclusive(
@@ -75,6 +78,10 @@ public class MealRestController {
                 atEndOfDayOrMax(endDate, endTime),
                 userId);
         return getMealsWithExcess(mealsDateFiltered,
-                startTime, endTime, authUserCaloriesPerDay());
+                startTime, endTime, securityUtil.getAuthUserCaloriesPerDay());
+    }
+
+    private Integer getUserId() {
+        return securityUtil.getAuthUserId();
     }
 }
---
+++ src/main/java/by/ttre16/enterprise/controller/rest/ProfileRestController.java
@@ -0,0 +1,8 @@
+package by.ttre16.enterprise.controller.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rest/profile")
+public class ProfileRestController { }
