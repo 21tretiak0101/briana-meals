084568c8990616f6424908972d793a8c972e394b
21tretiak0101 <ilia.tretiak.2001@mail.ru>
2020-09-01 23:45:34 +0300

    test: refactor rest controller tests

--- src/test/java/by/ttre16/enterprise/controller/rest/MealRestControllerTest.java
+++ src/test/java/by/ttre16/enterprise/controller/rest/MealRestControllerTest.java
@@ -12,6 +12,7 @@ import java.util.ArrayList;
 
 import static by.ttre16.enterprise.controller.util.json.JsonUtil.*;
 import static by.ttre16.enterprise.data.MealTestData.*;
+import static by.ttre16.enterprise.data.MealToTestData.map;
 import static by.ttre16.enterprise.data.TestData.ADMIN_ID;
 import static by.ttre16.enterprise.util.web.UrlUtil.MEAL_REST_URL;
 import static org.springframework.test.web.servlet
@@ -27,20 +28,27 @@ public class MealRestControllerTest extends AbstractRestControllerTest {
     @Autowired
     private MealService mealService;
 
+    @Override
+    public Integer getTestUserId() {
+        return ADMIN_ID;
+    }
+
     @Test
     public void getAll() throws Exception {
-        perform(get(MEAL_REST_URL))
+        perform(get(MEAL_REST_URL)
+                .header(authorizationHeader, bearerToken))
                 .andExpect(status().isOk())
                 .andExpect(content()
                         .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
                 .andExpect(MEAL_TEST_MATCHER.contentJson(
-                        MEALS.get(ADMIN_ID).values()))
+                        MEALS.get(getTestUserId()).values()))
                 .andDo(print());
     }
 
     @Test
-    public  void create() throws Exception {
+    public void create() throws Exception {
         MvcResult response = perform(post(MEAL_REST_URL)
+                .header(authorizationHeader, bearerToken)
                     .contentType(MediaType.APPLICATION_JSON)
                     .content(writeValue(getNew())))
                 .andExpect(status().isCreated())
@@ -50,28 +58,35 @@ public class MealRestControllerTest extends AbstractRestControllerTest {
         Integer mid = savedMeal.getId();
         newMeal.setId(mid);
         assertMatch(newMeal, savedMeal);
-        assertMatch(newMeal, mealService.getOne(ADMIN_ID, mid));
+        assertMatch(newMeal, mealService.getOne(getTestUserId(), mid));
     }
 
     @Test
     public void update() throws Exception {
         perform(put(MEAL_REST_URL + "/" + MEAL5_ID)
+                .header(authorizationHeader, bearerToken)
             .contentType(MediaType.APPLICATION_JSON)
-                .content(writeValue(getUpdated(ADMIN_ID, MEAL5_ID))))
+                .content(
+                        writeValue(map(getUpdated(getTestUserId(), MEAL5_ID)))
+                )
+        )
         .andExpect(status().isNoContent())
         .andDo(print());
-        assertMatch(getUpdated(ADMIN_ID, MEAL5_ID),
-                mealService.getOne(ADMIN_ID, MEAL5_ID));
+        assertMatch(
+                getUpdated(getTestUserId(), MEAL5_ID),
+                mealService.getOne(getTestUserId(), MEAL5_ID)
+        );
     }
 
     @Test
     public void deleteOne() throws Exception {
-        perform(delete(MEAL_REST_URL + "/" + MEAL5_ID))
+        perform(delete(MEAL_REST_URL + "/" + MEAL5_ID)
+                .header(authorizationHeader, bearerToken))
                 .andExpect(status().isNoContent())
                 .andDo(print());
         ArrayList<Meal> adminMeals = new ArrayList<>();
-        adminMeals.add(MEALS.get(ADMIN_ID).get(MEAL6_ID));
-        adminMeals.add(MEALS.get(ADMIN_ID).get(MEAL7_ID));
-        assertMatch(adminMeals, mealService.getAllByUserId(ADMIN_ID));
+        adminMeals.add(MEALS.get(getTestUserId()).get(MEAL6_ID));
+        adminMeals.add(MEALS.get(getTestUserId()).get(MEAL7_ID));
+        assertMatch(adminMeals, mealService.getAllByUserId(getTestUserId()));
     }
 }
--- src/test/java/by/ttre16/enterprise/controller/rest/ProfileRestControllerTest.java
+++ src/test/java/by/ttre16/enterprise/controller/rest/ProfileRestControllerTest.java
@@ -1,5 +1,6 @@
 package by.ttre16.enterprise.controller.rest;
 
+import by.ttre16.enterprise.data.UserInfoTestData;
 import by.ttre16.enterprise.model.User;
 import by.ttre16.enterprise.service.UserService;
 import org.junit.Test;
@@ -7,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 
 import static by.ttre16.enterprise.controller.util.json.JsonUtil.writeValue;
-import static by.ttre16.enterprise.data.TestData.ADMIN_ID;
+import static by.ttre16.enterprise.data.UserInfoTestData.*;
 import static by.ttre16.enterprise.data.UserTestData.*;
 import static by.ttre16.enterprise.util.web.UrlUtil.PROFILE_REST_URL;
 import static java.util.Collections.singletonList;
@@ -23,32 +24,47 @@ public class ProfileRestControllerTest extends AbstractRestControllerTest {
     @Autowired
     private UserService userService;
 
+    @Override
+    public Integer getTestUserId() {
+        return USER_ID;
+    }
+
     @Test
     public void getOne() throws Exception {
-        perform(get(PROFILE_REST_URL))
+        perform(get(PROFILE_REST_URL)
+                .header(authorizationHeader, bearerToken))
                 .andExpect(status().isOk())
                 .andExpect(content()
                         .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-                .andExpect(USER_TEST_MATCHER.contentJson(USERS.get(ADMIN_ID)))
+                .andExpect(USER_INFO_TEST_MATCHER
+                        .contentJson(
+                                map(USERS.get(getTestUserId()))
+                        )
+                )
                 .andDo(print());
     }
 
     @Test
     public void update() throws Exception {
-        User updated = getUpdated(ADMIN_ID);
+        User updated = getUpdated(getTestUserId());
         perform(put(PROFILE_REST_URL)
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .content(writeValue(updated)))
+                .header(authorizationHeader, bearerToken)
+                .contentType(MediaType.APPLICATION_JSON)
+                    .content(writeValue(UserInfoTestData.map(updated))))
                 .andExpect(status().isNoContent())
                 .andDo(print());
-        assertMatch(updated, userService.get(ADMIN_ID));
+        assertMatch(updated, userService.get(getTestUserId()));
     }
 
     @Test
     public void deleteOne() throws Exception {
-        perform(delete(PROFILE_REST_URL))
+        perform(delete(PROFILE_REST_URL)
+                .header(authorizationHeader, bearerToken))
                 .andExpect(status().isNoContent())
                 .andDo(print());
-        assertMatch(singletonList(USERS.get(USER_ID)), userService.getAll());
+        assertMatch(
+                singletonList(USERS.get(ADMIN_ID)),
+                userService.getAll()
+        );
     }
 }
