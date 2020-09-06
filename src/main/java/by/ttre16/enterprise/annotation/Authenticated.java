b6d49553a2f1ebe850ac6028a2851ea7dacc1063
21tretiak0101 <ilia.tretiak.2001@mail.ru>
2020-09-06 20:40:00 +0300

    refactor: change types target

--- src/main/java/by/ttre16/enterprise/annotation/Authenticated.java
+++ src/main/java/by/ttre16/enterprise/annotation/Authenticated.java
@@ -7,7 +7,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target({ElementType.PARAMETER, ElementType.TYPE})
+@Target({ ElementType.PARAMETER, ElementType.ANNOTATION_TYPE })
 @Retention(RetentionPolicy.RUNTIME)
 @AuthenticationPrincipal
 public @interface Authenticated { }
